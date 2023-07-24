475f3d4e01288c786079406c9b42dab2f18d8d66
jaykingamez <jaykinchan@gmail.com>
2023-07-24 14:26:25 +0800

    Add object type

---
+++ mygame/enums/object_type.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+class ObjType(Enum):
+    
+    WEAPON = "weapon"
+    ARMOR = "armor"
+    SHIELD = "shield"
+    HELMET = "helmet"
+    CONSUMABLE = "consumable"
+    GEAR = "gear"
+    QUEST = "quest"
+    TREASURE = "treasure"
